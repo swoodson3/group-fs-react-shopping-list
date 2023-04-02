33581206791815b6ccdcdb0177c36a8635aa9698
Everett Butler <everettjamesbutler@gmail.com>
2023-04-02 17:48:59 -0500

    got a start on my card function for the shopping list!

--- src/components/ShoppingList/ShoppingItem.jsx
+++ src/components/ShoppingList/ShoppingItem.jsx
@@ -1,4 +1,14 @@
+import React from 'react';
 import axios from 'axios';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+
 
 function ShoppingItem({ item, fetchItemList }) {
     const removeItem = (e) => {
@@ -31,11 +41,13 @@ function ShoppingItem({ item, fetchItemList }) {
 
 
     return (
-        <li key={item.id}>
-            {item.name} - {item.quantity}
-            <button onClick={(e) => markItem(e)}>BUY</button>
-            <button onClick={(e) => removeItem(e)}>Delete</button>
-        </li>
+        <Grid item xs={12} md={4}>
+            <Card xs={{minWidth: 275}}>
+                {item.name} - {item.quantity}
+                <button onClick={(e) => markItem(e)}>BUY</button>
+                <button onClick={(e) => removeItem(e)}>Delete</button>
+            </Card>
+        </Grid>
     )
 
 }
